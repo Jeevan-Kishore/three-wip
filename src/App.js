031663a1a37116d3c3ddc2fbbde5eaac30824a20
jeevan-kishore <kishor080@gmail.com>
2020-05-09 21:21:19 +0530

    feat(prettier): Added prettier :cop:

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
-import React, {useEffect, useRef} from 'react';
-import './App.css';
-import * as THREE from 'three/build/three.module';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+import * as THREE from "three/build/three.module";
 
 let camera, scene, renderer;
 let geometry, material, mesh;
 
-
-
 const init = (rootRef) => {
-
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
     camera.position.z = 1;
 
     scene = new THREE.Scene();
-
-    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
     material = new THREE.MeshNormalMaterial();
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    rootRef.current.appendChild( renderer.domElement );
-
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    rootRef.current.appendChild(renderer.domElement);
 };
 
 const animate = () => {
-
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
 
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.02;
 
-    renderer.render( scene, camera );
-
+    renderer.render(scene, camera);
 };
 
 const App = () => {
@@ -43,9 +36,7 @@ const App = () => {
         init(rootRef);
         animate();
     }, []);
-  return (
-    <div ref={rootRef} id="app-root"/>
-  );
+    return <div ref={rootRef} id="app-root" />;
 };
 
 export default App;
