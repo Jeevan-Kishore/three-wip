1995f64cc9cbc97320972f046c3e86f6674cb1a8
jeevan-kishore <kishor080@gmail.com>
2020-05-09 19:55:02 +0530

    feat(three): Init three js :sparkles::zap:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef} from 'react';
 import './App.css';
+import * as THREE from 'three/build/three.module';
 
-function App() {
+let camera, scene, renderer;
+let geometry, material, mesh;
+
+
+
+const init = (rootRef) => {
+
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.z = 1;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    material = new THREE.MeshNormalMaterial();
+
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    rootRef.current.appendChild( renderer.domElement );
+
+};
+
+const animate = () => {
+
+    requestAnimationFrame( animate );
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render( scene, camera );
+
+};
+
+const App = () => {
+    const rootRef = useRef(null);
+    useEffect(() => {
+        init(rootRef);
+        animate();
+    }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={rootRef} id="app-root">
     </div>
   );
-}
+};
 
 export default App;
